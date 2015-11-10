b02946d354de87691629adf160c0115ad062a185
Jack <catinacan@gmail.com>
2015-11-09 21:47:46 -1000

    save work

--- zed/Source.cpp
+++ zed/Source.cpp
@@ -318,7 +318,6 @@ int main(int argc, char **argv) {
 		cv::cvtColor(leftDetectMat, detectGray, CV_RGB2GRAY);
 		cv::vector<cv::Rect> faces;
 		cv::vector<cv::Rect> eyes;
-		face_cascade.detectMultiScale(detectGray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
 
 		//for (int i = 0; i < faces.size(); ++i){
 		//cv::rectangle(leftDetectMat, faces.at(i), (255, 0, 0), 2);
@@ -329,24 +328,39 @@ int main(int argc, char **argv) {
 		//	cv::rectangle(roi_color, eyes.at(i), (0, 255, 0), 2);
 		//}
 
+		const static cv::Scalar colors[] = { CV_RGB(0, 0, 255),
+			CV_RGB(0, 128, 255),
+			CV_RGB(0, 255, 255),
+			CV_RGB(0, 255, 0),
+			CV_RGB(255, 128, 0),
+			CV_RGB(255, 255, 0),
+			CV_RGB(255, 0, 0),
+			CV_RGB(255, 0, 255) };
 
 		int i = 0;
+		float scale = 1.0;
+		cv::Mat gray, smallImg(cvRound(leftDetectMat.rows / scale), cvRound(leftDetectMat.cols / scale), CV_8UC1);
+		cv::cvtColor(leftDetectMat, gray, CV_RGB2GRAY);
+		cv::resize(gray, smallImg, smallImg.size(), 0, 0, cv::INTER_LINEAR);
+		cv::equalizeHist(smallImg, smallImg);
+		face_cascade.detectMultiScale(leftDetectMat, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
+
 		for (cv::vector<cv::Rect>::const_iterator r = faces.begin(); r != faces.end(); r++, i++){
 			cv::Mat smallImgROI;
 			cv::vector<cv::Rect> nestedObjects;
 			cv::Point center;
-			cv::Scalar color = CV_RGB(255, 0, 0);
+			cv::Scalar color = colors[i % 8];
 			int radius;
 
 			double aspect_ratio = (double)r->width / r->height;
 			if (0.75 < aspect_ratio && aspect_ratio < 1.3){
-				center.x = cvRound((r->x + r->width*0.5) * 1);
-				center.y = cvRound((r->y + r->height*0.5) * 1);
-				radius = cvRound((r->width + r->height)*0.25 * 1);
+				center.x = cvRound((r->x + r->width*0.5) * scale);
+				center.y = cvRound((r->y + r->height*0.5) * scale);
+				radius = cvRound((r->width + r->height)*0.25 * scale);
 				circle(leftDetectMat, center, radius, color, 3, 8, 0);
 			}
 			else{
-				rectangle(leftDetectMat, cvPoint(cvRound(r->x * 1), cvRound(r->y * 1)), cvPoint(cvRound((r->x + r->width - 1) * 1), cvRound((r->y + r->height - 1) * 1)), color, 3, 8, 0);
+				rectangle(leftDetectMat, cvPoint(cvRound(r->x * scale), cvRound(r->y * scale)), cvPoint(cvRound((r->x + r->width - 1) * scale), cvRound((r->y + r->height - 1) * scale)), color, 3, 8, 0);
 			}
 		}
 
